eaa9bf49e050ac307a5d466d475b74c8b41fb703
Adir <adir.dayan@jmarquardt.com>
2025-03-17 19:21:49 +0100

    remove not found

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
-import NotFound from './NotFound';
 import Details from './Details';
+// import NotFound from './NotFound';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/details/:id/:title" element={<Details />} />
-      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
