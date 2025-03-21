cc9385ae342c4071f3a99a2b9502aaaab20b66e3
Adir <adir.dayan@jmarquardt.com>
2025-03-21 10:07:51 +0100

    update scroller

--- src/ScrollNotifier.jsx
+++ src/ScrollNotifier.jsx
@@ -2,25 +2,25 @@ import { useEffect } from "react";
 
 const ScrollNotifier = () => {
   useEffect(() => {
-    const handleScroll = () => {
+    const handleTouchMove = (event) => {
       const iframeDoc = document.documentElement;
       const scrollTop = iframeDoc.scrollTop;
       const scrollHeight = iframeDoc.scrollHeight;
       const clientHeight = window.innerHeight;
 
       if (scrollTop + clientHeight >= scrollHeight - 1) {
-        // Notify parent that iframe has reached the bottom
-        window.parent.postMessage({ type: "iframeReachedBottom" }, "*");
+        // Notify parent that iframe reached the bottom
+        window.parent.postMessage("iframeReachedBottom", "*");
 
-        // Prevent further scrolling inside the iframe
+        // Prevent further scrolling inside the iframe (fix for iOS)
         document.body.style.overflow = "hidden";
       } else {
         document.body.style.overflow = "auto";
       }
     };
 
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
+    window.addEventListener("touchmove", handleTouchMove, { passive: false });
+    return () => window.removeEventListener("touchmove", handleTouchMove);
   }, []);
 
   return null;
