079ef7eeea5982a306f54648bc4b74104ce0f264
Adir <adir.dayan@jmarquardt.com>
2025-03-21 10:51:42 +0100

    update

--- src/ScrollNotifier.jsx
+++ src/ScrollNotifier.jsx
@@ -4,7 +4,7 @@ const ScrollNotifier = () => {
     useEffect(() => {
         const handleTouchEnd = () => {
             const scrollableElement = document.documentElement || document.body;
-            const atBottom = scrollableElement.scrollHeight - scrollableElement.scrollTop <= scrollableElement.clientHeight + 1;
+            const atBottom = scrollableElement.scrollHeight - scrollableElement.scrollTop <= scrollableElement.clientHeight + 5;
 
             if (atBottom) {
                 window.parent.postMessage({ type: "iframeReachedBottom" }, "*");
