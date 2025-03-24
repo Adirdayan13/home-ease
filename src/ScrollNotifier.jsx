554f34e495864420f05de9752a1bf4f8be8506dd
Adir <adir.dayan@jmarquardt.com>
2025-03-24 22:38:57 +0100

    scroller

--- src/ScrollNotifier.jsx
+++ src/ScrollNotifier.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 const ScrollNotifier = () => {
     useEffect(() => {
-        const handleTouchEnd = () => {
+        const handleScrollOrTouchEnd = () => {
             const scrollableElement = document.documentElement || document.body;
             const atBottom = scrollableElement.scrollHeight - scrollableElement.scrollTop <= scrollableElement.clientHeight + 5;
 
@@ -11,8 +11,12 @@ const ScrollNotifier = () => {
             }
         };
 
-        window.addEventListener("touchend", handleTouchEnd);
-        return () => window.removeEventListener("touchend", handleTouchEnd);
+        window.addEventListener("touchend", handleScrollOrTouchEnd);
+        window.addEventListener("scroll", handleScrollOrTouchEnd);
+        return () => {
+            window.removeEventListener("touchend", handleScrollOrTouchEnd);
+            window.removeEventListener("scroll", handleScrollOrTouchEnd);
+        };
     }, []);
 
     return null;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter as Router } from 'react-router-dom'
+import ScrollNotifier from './ScrollNotifier';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -11,6 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
+      <ScrollNotifier />
       <App />
     </Router>
   </React.StrictMode>
