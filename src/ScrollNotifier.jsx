148623200d9d06907448f5cc8c3f1c03056dc70c
Adir <adir.dayan@jmarquardt.com>
2025-03-25 21:18:48 +0100

    timeout

--- src/ScrollNotifier.jsx
+++ src/ScrollNotifier.jsx
@@ -3,11 +3,14 @@ import { useEffect } from "react";
 const ScrollNotifier = () => {
     useEffect(() => {
         const handleScrollOrTouchEnd = () => {
-            const scrollableElement = document.body;
+            const scrollableElement = document.documentElement; // Changed from document.body to document.documentElement
             const atBottom = scrollableElement.scrollHeight - scrollableElement.scrollTop <= scrollableElement.clientHeight + 5;
             console.log('adir', { scrollableElement, atBottom})
             if (atBottom) {
                 window.parent.postMessage({ type: "iframeReachedBottom" }, "*");
+                setTimeout(() => {
+                    window.parent.postMessage({ type: "iframeReachedBottom" }, "*");
+                }, 100); // Added a slight delay
             }
         };
 
