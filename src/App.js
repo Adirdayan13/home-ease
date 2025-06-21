d7a60496ac207d15f652c5289dfb397e0962d746
Adir <adir.dayan@jmarquardt.com>
2025-06-21 21:04:30 +0200

    add language listener

--- src/App.js
+++ src/App.js
@@ -5,18 +5,37 @@ import SearchProfile from './search-profile/SearchProfile';
 import './App.css';
 import useSWRImmutable from 'swr/immutable';
 import { fetcher } from './utils';
+import { useEffect, useState } from 'react';
 
 function App() {
   const { data: brokers = [] } = useSWRImmutable(
     `https://api.propstack.de/v1/brokers`,
     fetcher
   );
+  const [language, setLanguage] = useState(null);
+
+
+    useEffect(() => {
+    // Ask parent for the Weglot language
+    window.parent.postMessage("getWeglotLang", "*");
+  
+    function handleMessage(event) {
+      const { wglang } = event.data;
+      if (wglang) {
+        console.log("Weglot language from parent:", wglang);
+        setLanguage(wglang)
+      }
+    }
+  
+    window.addEventListener("message", handleMessage);
+    return () => window.removeEventListener("message", handleMessage);
+  }, []);
 
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/details/:id/:title" element={<Details brokers={brokers} />} />
-      <Route path="/suche-profil" element={<SearchProfile />} />
+      <Route path="/" element={<Home language={language} />} />
+      <Route path="/details/:id/:title" element={<Details brokers={brokers} language={language} />} />
+      <Route path="/suche-profil" element={<SearchProfile language={language} />} />
     </Routes>
   );
 }
